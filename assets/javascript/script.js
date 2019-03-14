c38e54f01a77c188cf4a773dfd1e9ef635008160
ksknight <korvette_00Whotmail.com>
2019-03-13 21:30:10 -0600

    update code 3-13-19

---
+++ assets/javascript/script.js
@@ -0,0 +1,100 @@
+// VARIABLES
+var searchVal;
+var topics = ["Thor", "Ironman", "Star Lord", "Drax", "Captain America", "Dr. Strange", "Spiderman", "Black Panther"];
+var apiKey = "61JxIUAPGbJxUuy3n1QP5294P0Bt9EDh";
+var status = "still";
+var displayedGIF = 10;
+
+
+
+
+// FUNCTIONS
+// AJAX
+
+
+function createButton() {
+    for (var i = 0; i < topics.length; i++) {
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=" + apiKey;
+        newGif = $("<button>");
+        newGif.text(topics[i]);
+        newGif.attr("class", "buttons")
+        newGif.attr("data-URL", queryURL);
+        $(".gifButtons").append(newGif);
+    }
+};
+
+// FUNCTION FOR submit
+$(".submit").on("click", function () {
+    event.preventDefault();
+    searchVal = $(".searchBar").val().trim();
+    topics.push(searchVal)
+    $(".gifButtons").empty();
+    createButton();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchVal + "&api_key=" + apiKey;
+    $(".gifsImages").empty();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 0; i < displayedGIF; i++) {
+            var gifURL = response.data[i].images.fixed_height.url;
+            var stillGifURL = response.data[i].images.fixed_height_still.url;
+            var rating = response.data[i].rating;
+            var newGif = $("<div>");
+            newGif.html("<p>Rating: " + rating + "</p><img src='" + response.data[i].images.fixed_height_still.url + "'/>")
+            newGif.attr("data-state", status);
+            newGif.attr("data-stillURL", stillGifURL);
+            newGif.attr("data-gifURL", gifURL);
+            newGif.html();
+            $(".gifsImages").append(newGif);
+        }
+    });
+    $(".searchBar").val("");
+})
+
+
+
+
+// display GIFs function
+$(".gifButtons").on("click", ".buttons", function () {
+    var queryURL = $(this).attr("data-URL");
+    $(".gifsImages").empty();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 0; i < displayedGIF; i++) {
+            var gifURL = response.data[i].images.fixed_height.url;
+            var stillGifURL = response.data[i].images.fixed_height_still.url;
+            var rating = response.data[i].rating;
+            var newGif = $("<div>");
+            newGif.html("<p>Rating: " + rating + "</p><img src='" + response.data[i].images.fixed_height_still.url + "'/>")
+            newGif.attr("data-state", status);
+            newGif.attr("data-stillURL", stillGifURL);
+            newGif.attr("data-gifURL", gifURL);
+            newGif.html();
+            $(".gifsImages").append(newGif);
+
+        }
+    });
+
+})
+$(".gifsImages").on("click", "#gif", function () {
+    var state = $(this).attr("data-state");
+    var gifURL = $(this).attr("data-gifURL");
+    var stillGifURL = $(this).attr("data-stillURL");
+
+    if (state === "still") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", gifURL);
+    }
+    else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", stillGifURL);
+    }
+
+})
+
+createButton()
+
